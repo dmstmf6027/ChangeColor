090fe30578ff9baf272c92e8d2400e5bfe04144a
201631053 <hubot@githhub@com>
2018-03-14 15:01:52 +0900

    add text

--- ChangeColor/ViewController.swift
+++ ChangeColor/ViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var index : Int = 0
+    @IBOutlet weak var yellow: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +22,24 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    @IBAction func btChange(_ sender: Any) {
+        if(index == 0) {
+            view.backgroundColor = UIColor.cyan
+            index = index + 1
+            yellow.text = "cyan"
+        }
+        else if(index == 1){
+            view.backgroundColor = UIColor.blue
+            index = index + 1
+            yellow.text = "blue"
+        }
+        else if(index == 2){
+            view.backgroundColor = UIColor.yellow
+            index = index - 2
+            yellow.text = "yellow"
+        }
+    
 }
 
+}
